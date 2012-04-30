48800daaa6dd34fd248b71da9f09f1f06ea7862c
edipotrebol <edipotrebol@gmail.com>
2012-04-30 21:24:51 +0200

    Pruebas en el controlador de usuario

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -4,6 +4,7 @@ class Users extends CI_Controller {
 
 	public function __construct() {
 	   parent::__construct();
+	   $this->load->spark('gravatar/1.1.1');
 	}
 
 	public function index() {
@@ -11,6 +12,7 @@ class Users extends CI_Controller {
 		$password = '12345678';
 		$email = 'benedmunds';
 		$this->ion_auth->register($username, $password, $email);*/
+		echo '<img src="' . Gravatar_helper_from_email('edipotrebol@gmail.com') . '">';
 	}
 
 }
