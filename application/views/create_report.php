6fc07378b9563566858d07891cdfbefc1b8d94ad
Daniel Aguilar <dacmail@gmail.com>
2012-05-30 12:43:25 +0200

    Estilos de desarrollo para el resto de páginas

--- application/views/create_report.php
+++ application/views/create_report.php
@@ -1,13 +1,18 @@
-<h1>Enviar nuevo reporte</h1>
-<div class="validation_errors">
-	<?php echo validation_errors(); ?>
-	<?=$error_url_check ?>
-</div>
-<?php echo form_open($this->router->reverseRoute('reports-send_url')) ?>
+<div id="container" class="sending clearfix">
+	<div id="content">
+		<h1 class="title">Enviar nuevo reporte</h1>
+		<div class="validation_errors">
+			<?php echo validation_errors(); ?>
+			<?=$error_url_check ?>
+		</div>
+		<?php echo form_open($this->router->reverseRoute('reports-send_url')) ?>
 
-	<label for="title">Url de la noticia</label>
-	<input type="input" name="url" /><br />
+			<label class="label" for="title">Url de la noticia</label>
+			<input class="text" type="input" placeholder="http://ejemplo.com/noticia.html" name="url" /><br />
 
-	<input type="submit" name="submit" value="Siguiente" /> 
+			<input class="submit button" type="submit" name="submit" value="Siguiente" /> 
 
-</form>
+		</form>
+	</div>
+</div>	
+
--- application/views/list_reports.php
+++ application/views/list_reports.php
@@ -1,5 +1,10 @@
-<ul>
-	<? foreach ($reports as $report) : ?>
-		<li><?= anchor($this->router->reverseRoute('reports-view', array('slug' => $report->slug)), $report->title); ?></li>
-	<? endforeach; ?>
-</ul>
+<div id="container" class="clearfix">
+	<div id="content">
+		<h1 class="title">Listado de reportes</h1>
+		<ul class="reports_list">
+			<? foreach ($reports as $report) : ?>
+				<li><?= anchor($this->router->reverseRoute('reports-view', array('slug' => $report->slug)), $report->title); ?></li>
+			<? endforeach; ?>
+		</ul>
+	</div>
+</div>
