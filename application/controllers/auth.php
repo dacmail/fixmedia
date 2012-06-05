1693c5c29c6ddd3953e0d946475aca6e6cad0e4d
Daniel Aguilar <dacmail@gmail.com>
2012-06-05 19:31:40 +0200

    Copia del controlador AUTH al directorio de la app

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -346,11 +346,6 @@ class Auth extends CI_Controller {
 	{
 		$this->data['title'] = "Create User";
 
-		if (!$this->ion_auth->logged_in() || !$this->ion_auth->is_admin())
-		{
-			redirect('auth', 'refresh');
-		}
-
 		//validate form input
 		$this->form_validation->set_rules('first_name', 'First Name', 'required|xss_clean');
 		$this->form_validation->set_rules('last_name', 'Last Name', 'required|xss_clean');
