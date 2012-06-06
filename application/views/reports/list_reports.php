e61812284dbc5d135d11adbd235258d20e981aa0
Daniel Aguilar <dacmail@gmail.com>
2012-06-06 11:29:02 +0200

    Maquetación creación reporte y home

--- application/views/reports/list_reports.php
+++ application/views/reports/list_reports.php
@@ -1,6 +1,6 @@
 <div id="container" class="clearfix">
 	<div id="content">
-		<h1 class="title">Listado de reportes</h1>
+		<h1 class="title">Listado de reportes <? echo anchor($this->router->reverseRoute('reports-create'), 'Añade un nuevo reporte', array('class' => "button submit")); ?></h1>
 		<ul class="reports_list">
 			<? foreach ($reports as $report) : ?>
 				<li><?= anchor($this->router->reverseRoute('reports-view', array('slug' => $report->slug)), $report->title); ?></li>
