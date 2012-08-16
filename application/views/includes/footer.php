12d90103620440791cd7bb6bab56d9bde35bb5cc
Daniel Aguilar <dacmail@gmail.com>
2012-08-16 11:12:52 +0200

    Maquetación footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,3 +1,59 @@
+<footer id="footer">
+	<section class="main-footer">
+		<div class="wrap clearfix">
+			<div class="block">
+				<p class="logo"><a href="#">Fixmedia</a></p>
+				<p>fixmedia.org es un proyecto de <a href="http://nxtmdia.com" target="_blank">NXTMDIA</a> para el procomún financiado a través de <a href="http://goteo.org" target="_blank">Goteo.org</a> por <a href="#">estas personas.</a></p>
+			</div>
+
+			<nav class="clearfix menus">
+				<ul class="menu">
+					<li class="name">Menú</li>
+					<li><a href="#">Portada</a></li>
+					<li><a href="#">Más urgentes</a></li>
+					<li><a href="#">Pendientes</a></li>
+					<li><a href="#">Recientes</a></li>
+					<li><a href="#">Top usuarios</a></li>
+					<li><a href="#">Top fuentes</a></li>
+					<li><a href="#">Estadísticas</a></li>
+				</ul>
+				<ul class="menu">
+					<li class="name">Social</li>
+					<li><a href="#">Registro</a></li>
+					<li><a href="#">Entrar</a></li>
+					<li><a href="#">Usa nuestro marcador</a></li>
+					<li><a href="#">¿Eres periodista?</a></li>
+					<li><a href="#">¿Creas contenido?</a></li>
+				</ul>
+				<ul class="menu">
+					<li class="name">Manual</li>
+					<li><a href="#">Qué es fixmedia</a></li>
+					<li><a href="#">Cómo funciona</a></li>
+					<li><a href="#">Qué queremos conseguir</a></li>
+					<li><a href="#">Cofinanciadores</a></li>
+					<li><a href="#">FAQ</a></li>
+					<li><a href="#">Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</section>
+	<section class="logos-footer">
+		<div class="wrap clearfix">
+			<div class="develop">
+				<h3 class="title">Desarrollado por:</h3>
+				<img src="<?= base_url(); ?>/images/logos/nxt.jpg"/>
+			</div>
+			<div class="cofin">
+				<h3 class="title">Cofinanciado por:</h3>
+				<img src="<?= base_url(); ?>/images/logos/cof1.jpg"/> 
+				<img src="<?= base_url(); ?>/images/logos/cof2.jpg"/>  
+				<img src="<?= base_url(); ?>/images/logos/cof3.jpg"/> 
+				<img src="<?= base_url(); ?>/images/logos/cof4.jpg"/>
+			</div>
+
+		</div>
+	</section>
+</footer>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
