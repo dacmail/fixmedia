2ab8189cb1eb8621f89c66ba5747e563d42d0f94
Daniel Aguilar <dacmail@gmail.com>
2012-08-21 12:59:35 +0200

    Maquetación del perfil de usuario

--- application/views/user/user.php
+++ application/views/user/user.php
@@ -1,6 +1,16 @@
 <div id="container" class="clearfix columns">
 	<div id="content">
-		<h1 class="title"><?= $user->username; ?></h1>
+		<section class="user_info clearfix">
+			  <?=gravatar( $the_user->email, 150 )?>
+			  <div class="data">
+			  		<h1 class="name"><?= $user->username; ?></h1>
+			  		<p class="when">Mejorando noticias desde el <?= date('d/m/Y', $user->created_on); ?></p>
+			  		<p class="bio">Nunc vel turpis, vut habitasse enim rhoncus nec ridiculus duis purus amet, duis dapibus enim, elementum scelerisque ac? Porta? Magnis? Vut a nisi adipiscing eros quis odio! Lorem odio penatibus, sit!</p>
+			 		<p class="url">Web: <a href="#">http://google.com/</a></p>
+			  </div>
+			  
+		</section>
+		
 	</div>
-	<?php $this->load->view('includes/sidebar'); ?>
+	<?php $this->load->view('includes/sidebar-user'); ?>
 </div>
