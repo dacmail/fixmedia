0e78ffc3cf998b6689f91f499dc7c7deccaab65f
Daniel Aguilar <dacmail@gmail.com>
2012-09-06 17:41:46 +0200

    Popup compartir reporte

---
+++ application/views/services/share.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/styles.css" />
+</head>
+<body>
+	<div class="popup">
+		<a href="<?=$url?>">Compartir en Facebook</a>
+		<a href="<?=$url?>">Compartir en Twitter</a>
+		<a href="<?=$url?>">Compartir en Google+</a>
+	</div>
+</body>
+</html>
