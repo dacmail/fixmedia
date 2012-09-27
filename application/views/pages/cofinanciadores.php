a148f3b4d0a5797e3a7ed28ff8312d5565baeed7
Daniel Aguilar <dacmail@gmail.com>
2012-09-27 10:34:20 +0200

    Cofinanciadores

--- application/views/pages/cofinanciadores.php
+++ application/views/pages/cofinanciadores.php
@@ -1,13 +1,13 @@
-<div id="container" class="columns static clearfix">     
-	<div id="content">         
-		<h1 class="title">Cofinanciadores</h1>         
-		<p class="sub_title sep">Fixmedia ha sido posible gracias a la <a href="http://www.goteo.org/project/fixmedia.org-mejora-las-noticias/supporters">financiación colectiva por parte de 170 personas</a> a través de la plataforma de crowdfunding Goteo.org.</p>         
-		<p>Ellas no sólo han permitido que el equipo de <a href="http://nxtmdia.com">Nxtmdia</a> se pudiera poner a trabajar en el diseño 
-			y desarrollo de la herramienta, sino que han sido la primera comunidad de usuarios de Fixmedia, 
-			y la aportación de muchos de ellos en nuestros <a href="http://www.fixmedia.org/blog/category/la-fabrica/beta-testing/">testings</a> ha sido fundamental.</p>         
-		
+<div id="container" class="columns static clearfix">
+	<div id="content">
+		<h1 class="title">Cofinanciadores</h1>
+		<p class="sub_title sep">Fixmedia ha sido posible gracias a la <a href="http://www.goteo.org/project/fixmedia.org-mejora-las-noticias/supporters">financiación colectiva por parte de 170 personas</a> a través de la plataforma de crowdfunding Goteo.org.</p>
+		<p>Ellas no sólo han permitido que el equipo de <a href="http://nxtmdia.com">Nxtmdia</a> se pudiera poner a trabajar en el diseño
+			y desarrollo de la herramienta, sino que han sido la primera comunidad de usuarios de Fixmedia,
+			y la aportación de muchos de ellos en nuestros <a href="http://www.fixmedia.org/blog/category/la-fabrica/beta-testing/">testings</a> ha sido fundamental.</p>
+
 		<p>A todos ellos, <strong>gracias</strong></p>
-		
+
 			<iframe frameborder="0" height="540px" src="http://www.goteo.org/widget/wof/fixmedia.org-mejora-las-noticias/580" width="640px" scrolling="no"></iframe>
 
 		<p>También queremos agradecer de forma especial a nuestros <strong>mecenas</strong>, aquellos cofinanciadores que a través de Goteo donaron 600 euros:</p>
@@ -19,6 +19,8 @@
 		</ul>
 
 	<p>Y también a <a href="http://www.irekia.euskadi.net/es">Irekia</a>, la plataforma de open government del Gobierno Vasco, cuya aportación (de 10.690 euros) va a posibilitar que Fixmedia siga desarrollando nuevas e importantes funcionalidades no previstas en la campaña de crowdfunding (entre ellas la importantísima API) y que se irán implementando durante los primeros meses de vida de Fixmedia. Os lo iremos contando, como todo, en <a href="http://www.fixmedia.org/blog">nuestro blog</a>.</p>
+	<p>Por último, queremos agradecer públicamente a la empresa de hosting <a href="http://comvive.es">Comvive</a> que nos ceda sin coste alguno un VPS donde alojar Fixmedia :)</p>
+
 	<p class="nav">Ir a... ‘<a href="<?= site_url($this->router->reverseRoute('statics', array('page' => 'que-es-fixmedia'))); ?>">Qué es fixmedia</a>’ o a la ‘<a href="<?= site_url($this->router->reverseRoute('statics', array('page' => 'como-funciona'))); ?>">FAQ</a>’</p>
 
 	</div>
