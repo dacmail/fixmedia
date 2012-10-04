dd8a29011fae81a9347ab01064fdd6e0e52cecf4
Daniel Aguilar <dacmail@gmail.com>
2012-10-04 10:41:57 +0200

    Nombre de usuario mal enlazado en report-info

--- application/views/includes/report-info.php
+++ application/views/includes/report-info.php
@@ -9,7 +9,7 @@
 	</div>
 	<h1 class="title"><?=$report->title;?></h1>
 	<div class="report_meta">
-		<p class="authorship">Enviado por <a href="<?= site_url($this->router->reverseRoute('user-profile', array('username' => $report->user->name))); ?>"><?= $report->user->name; ?></a> el <?= $report->created_at->format('d/m/Y'); ?></p>
+		<p class="authorship">Enviado por <a href="<?= site_url($this->router->reverseRoute('user-profile', array('username' => $report->user->username))); ?>"><?= $report->user->name; ?></a> el <?= $report->created_at->format('d/m/Y'); ?></p>
 		<p class="source">Fuente: <a href="<?= site_url($this->router->reverseRoute('source-profile', array('sitename' => $report->site))); ?>"><?= $report->site; ?></a></p>
 	</div>
 </section>
