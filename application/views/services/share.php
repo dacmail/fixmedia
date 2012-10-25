ab0ce39a7ebda6f10bb5160faabafde957730ab1
Daniel Aguilar <dacmail@gmail.com>
2012-10-25 10:50:18 +0200

    Botón twittear con recomendación

--- application/views/services/share.php
+++ application/views/services/share.php
@@ -10,10 +10,12 @@
 		<p class="text"><?=$content?></p>
 		<div class="share-wrap clearfix">
 			<a class="icon fb" target="_blank" href="http://www.facebook.com/sharer/sharer.php?u=<?=$url?>">Compartir en Facebook</a>
-			<a class="icon tw" target="_blank" href="https://twitter.com/intent/tweet?original_referer=<?=$url?>&text=Esta noticia necesita ser arreglada - '<?= substr(urlencode($report_title), 0, 40) ?>...'&url=<?=$url?>&via=fixmedia_org">Compartir en Twitter</a>
+			<a class="icon tw" target="_blank" href="https://twitter.com/intent/tweet?original_referer=<?=$url?>&related=fixmedia_org&text=Esta noticia necesita ser arreglada - '<?= substr(urlencode($report_title), 0, 40) ?>...'&url=<?=$url?>&via=fixmedia_org">Compartir en Twitter</a>
 			<a class="icon gp" target="_blank" href="https://plus.google.com/share?url=<?=$url?>">Compartir en Google+</a>
 		</div>
 
 	</div>
 </body>
 </html>
+
+
