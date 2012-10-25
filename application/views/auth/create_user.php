e6d4079a800f14b26302449dd3e29492bfee6420
Daniel Aguilar <dacmail@gmail.com>
2012-10-25 12:07:19 +0200

    Registro, login y recuperación de contraseña.
    Eliminación de las invitaciones y confirmar contraseña

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -1,39 +1,19 @@
 <div id="container" class="columns create_user sending login clearfix">
   <div id="content">
-
     <h1 class="title">Regístrate</h1>
-
-	<? if (!empty($message)) : ?><div id="infoMessage"><?= $message;?></div><? endif;?>
-
     <?php echo form_open("auth/create_user");?>
-
-      <p><label class="label">Código de invitación:</label>
-      <?php echo form_input($invitation_code);?>
-      </p>
-
+      <? if (!empty($message)) : ?><div id="infoMessage"><?= $message;?></div><? endif;?>
       <p><label class="label">Usuario:</label>
-      <?php echo form_input($username);?>
+        <?php echo form_input($username);?>
       </p>
-
       <p><label class="label">Email:</label>
-      <?php echo form_input($email);?>
+        <?php echo form_input($email);?>
       </p>
-
       <p><label class="label">Contraseña:</label>
-      <?php echo form_input($password);?>
+        <?php echo form_input($password);?>
       </p>
-
-      <p><label class="label">Confirma contraseña:</label>
-      <?php echo form_input($password_confirm);?>
-      </p>
-
-
-
-
       <p><?php echo form_submit('submit', 'Registrarse', 'class="submit button"');?></p>
-
-
     <?php echo form_close();?>
-
+    <div class="outer_box">¿Ya tienes cuenta? <a href="<?= site_url($this->router->reverseRoute('login')); ?>">Inicia sesión</a></div>
   </div>
 </div>
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -20,6 +20,6 @@
     <?php echo form_close();?>
 
     <p><a href="<?= site_url('auth/forgot_password'); ?>">¿Olvidaste tu contraseña?</a></p>
-    <div class="outer_box">¿Aún no tienes cuenta? <a href="<?= site_url('auth/forgot_password'); ?>">Regístrate</a></div>
+    <div class="outer_box">¿Aún no tienes cuenta? <a href="<?= site_url($this->router->reverseRoute('register')); ?>">Regístrate</a></div>
   </div>
 </div>
