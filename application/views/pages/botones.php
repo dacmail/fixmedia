29b32f92d579057b09ab1a026f3687b1c4a03c53
Daniel Aguilar <dacmail@gmail.com>
2012-11-15 11:23:14 +0100

    Pagina de Botones compartir

---
+++ application/views/pages/botones.php
@@ -0,0 +1,46 @@
+<div id="container" class="columns static clearfix">
+	<div id="content">
+		<h1 class="title">Fixmedia en tu web o blog</h1>
+		<p class="sub_title sep">Tu comunidad se preocupa por la calidad de lo que publicas: ayúdales a ayudarte con el botón de Fixmedia.</p>
+		<p>El botón de Fixmedia insertado al final de cada post o noticia que publiques en tu sitio web o en tu blog es una gran posibilidad para:</p>
+		<ul>
+			<li>Que la inteligencia colectiva te ayude a mejorar tus contenidos.</li>
+			<li>Que tus contenidos circulen y traigan tráfico a tu web.</li>
+			<li>Que tu comunidad de lectores se sienta más unida a tí y tus contenidos, ya que les estás abriendo la posibilidad de tomar parte en ellos.</li>
+		</ul>
+		<h2>Consigue tu botón ahora</h2>
+		<p class="sub_title">¡Es muy fácil!</p>
+		<table>
+			<tr>
+				<th class="first">WordPress</th>
+				<th>Cualquier sistema web</th>
+			</tr>
+
+			<tr>
+				<td class="first">
+					<p class="center"><img alt="WordPress" src="<?= base_url('images/logos/wordpress.jpg'); ?>"/></p>
+					<p>Si utilizas WordPress para publicar tu web, es extremadamente sencillo: </p>
+					<p><a target="_blank" class="button" href="http://wordpress.org/extend/plugins/boton-fixmedia">Descarga nuestro plugin</a></p>
+					<p>y sigue las instrucciones de instalación.</p>
+				</td>
+				<td>
+
+					<p>Sea cual sea tu web, puedes instalar el botón simplemente copiando y pegando el código del botón que desees (amarillo o gris)</p>
+					<p><iframe src="http://fixmedia.org/services/fixit?url=DIRECCION_PAGINA" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:23px;" allowTransparency="true"></iframe></p>
+					<p><textarea><iframe src="http://fixmedia.org/services/fixit?url=DIRECCION_PAGINA" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:23px;" allowTransparency="true"></iframe></textarea></p>
+
+					<p><iframe src="http://fixmedia.org/services/fixit?url=DIRECCION_PAGINA&style=gray" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:23px;" allowTransparency="true"></iframe></p>
+					<p><textarea><iframe src="http://fixmedia.org/services/fixit?url=DIRECCION_PAGINA" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:23px;" allowTransparency="true"></iframe></textarea></p>
+
+
+					<p><strong>IMPORTANTE</strong>: Debes sustituir la cadena <i>DIRECCION_PAGINA</i> del código del botón por la URL que quieras que se envíe a Fixmedia.</p>
+
+				</td>
+			</tr>
+		</table>
+
+		<p>Tener instalado el botón de Fixmedia en tu sitio web te permitirá tener prioridad para trabajar con nuestra API cuando esté disponible.</p>
+		<p>¿Tienes cualquier duda? Escríbenos a <a href="mailto:comunidad@fixmedia.org">comunidad@fixmedia.org</a></p>
+	</div>
+	<?php $this->load->view('includes/sidebar-static'); ?>
+</div>
